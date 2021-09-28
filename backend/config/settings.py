4fd6f6bc6119fa5dcf8b3b5085f83f78ac09ce46
Iqbal Honnur <iqbalhonnur@gmail.com>
2021-09-28 18:37:58 +0530

    commiting after many days

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^4f=(yxn314@+oh3x%-dabcr)aor%$clc3x5+51nn$9-lzt9h1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
