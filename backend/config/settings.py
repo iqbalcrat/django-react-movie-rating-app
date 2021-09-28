52066e0419753153057a7af73aec4d2c6b4fd7ca
Iqbal Honnur <iqbalhonnur@gmail.com>
2021-09-28 18:51:27 +0530

    optimized the json output

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^4f=(yxn314@+oh3x%-dabcr)aor%$clc3x5+51nn$9-lzt9h1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
