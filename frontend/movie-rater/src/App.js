a0d0a95f25e8cdcdadf368277a90985a611143ed
Iqbal Honnur <iqbalhonnur@gmail.com>
2021-09-29 19:05:12 +0530

    react app setup

--- frontend/movie-rater/src/App.js
+++ frontend/movie-rater/src/App.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
 
-  const [movies, setMovie] = useState(['Interstellar', 'Titanic', 'Inception'])
+  const [movies, setMovie] = useState([])
+
+
+  //Fetching the movie list and dumping it into movies variable above
+  useEffect(() => {
+
+    fetch('http://127.0.0.1:8000/api/movies', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Token 947924344ef25d12f37adfe31b8c98bc5deab39d"
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setMovie(resp))
+    .then(error => console.log(error))
+
+  }, [])
 
   return (
     <div className="App">
@@ -16,7 +33,8 @@ function App() {
       <div>
       { movies.map( movie => {
 
-        return <h2>{movie}</h2>
+        return <h2>{movie.title}</h2>
+ 
       })}
 
       </div>
